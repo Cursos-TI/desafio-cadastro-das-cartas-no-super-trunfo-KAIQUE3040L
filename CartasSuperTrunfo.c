918df84a4ebf5cf967897c6a2d6962f512b5ba7d
KAIQUE3040L <kaiquefernandes234@gmail.com>
2025-07-03 18:12:41 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,8 @@
     //Teste larissa
 
     int main() {
+        printf("Desafio Super Trunfo\n");
+        printf("Novo Commit\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
